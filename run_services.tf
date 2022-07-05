96db989c8c09293fd07df7c499de88642216aa72
mmmakarets <makarets_ak17@nuwm.edu.ua>
2022-07-05 11:50:30 +0000

    [+] add html volume

--- run_services.tf
+++ run_services.tf
@@ -12,7 +12,7 @@ resource "docker_image" "mysql" {
 }
 
 resource "docker_image" "web_demo" {
-  name = "mmmakarets/web-demo:1.0.6"
+  name = "mmmakarets/web-demo:1.1.0"
 }
 
 resource "docker_network" "web_demo" {
